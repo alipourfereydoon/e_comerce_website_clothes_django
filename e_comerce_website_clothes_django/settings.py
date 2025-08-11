d6fbdba16dc91e09177de2cdb5011a8f6cfddc3c
ali.pourfereydoon <pourfereydoonali@gmail.com>
2025-08-11 07:46:46 -0700

    create account app and customize user from panel admin

--- e_comerce_website_clothes_django/settings.py
+++ e_comerce_website_clothes_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'admin_persian',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -126,4 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+AUTH_USER_MODEL = "account.User"
